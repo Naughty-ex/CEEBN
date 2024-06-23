548b949b67f5eb98f6bf21a448fd0540eb64bcdd
thatnoahboy <noahakexander@gmail.com>
2024-06-23 12:49:41 +0100

    place order button now working for laptop

--- script/redirect.js
+++ script/redirect.js
@@ -16,14 +16,54 @@ function redirectToAccessories() {
   window.location.href = "../html/accessories.html";
 }
 
+// redirect to iPhones
+
+function redirectToIphones() {
+  window.location.href = "../html/iphones.html";
+}
+
 // redirect to home
 
 function redirectToHome() {
   window.location.href = "../html/index.html";
 }
 
+
+// CONSOLE PAGE
 // redirect to PS5 Pad 
 
 function redirectPs5Pad() {
   window.location.href = "../html/placeorder.html";
+}
+
+// LAPTOP PAGE
+
+// redirect to card1
+
+function redirectCard1() {
+  window.location.href = "../laptop place order html/card1.html";
+}
+
+// redirect to card2
+
+function redirectCard2() {
+  window.location.href = "../laptop place order html/card2.html";
+}
+
+// redirect to card3
+
+function redirectCard3() {
+  window.location.href = "../laptop place order html/card3.html";
+}
+
+// redirect to card4
+
+function redirectCard4() {
+  window.location.href = "../laptop place order html/card4.html";
+}
+
+// redirect to card4
+
+function redirectCard5() {
+  window.location.href = "../laptop place order html/card5.html";
 }
